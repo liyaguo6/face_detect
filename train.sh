eb214ebf158a401f658252bd3d48e5cb34b2cc87
李亚国 <1075543143@qq.com>
2019-01-03 21:33:32 +0800

    网络训练启动文件

---
+++ train.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+D:/caffe-master/Build/x64/Debug/caffe.exe train --solver=D:/face_detect-master/solver.prototxt #\
+#--weights=models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
+#--gpu 0 
